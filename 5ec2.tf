24e65749c369c8dd660d6604a23e52dcc637af42
pillakarthik4 <karthikpilla51@gmail.com>
2023-05-15 09:46:29 +0530

    created server

--- 5ec2.tf
+++ 5ec2.tf
@@ -1,18 +1,16 @@
 resource "aws_instance" "web-1" {
-  ami = var.imagename
-  resource "aws_instance" "web-1" {
-    ami                         = var.imagename
-    availability_zone           = "us-east-1a"
-    instance_type               = var.instance_type
-    key_name                    = "Karthik"
-    subnet_id                   = aws_subnet.subnet1-public.id
-    vpc_security_group_ids      = ["${aws_security_group.allow_all_traffic.id}"]
-    associate_public_ip_address = true
-    tags = {
-      Name       = "Server-1"
-      Env        = "Prod"
-      Owner      = "Sree"
-      CostCenter = "ABCD"
-    }
+  ami                         = var.imagename
+  availability_zone           = "us-east-1a"
+  instance_type               = var.instance_type
+  key_name                    = "Karthikkey"
+  subnet_id                   = aws_subnet.subnet1-public.id
+  vpc_security_group_ids      = ["${aws_security_group.allow_all_traffic.id}"]
+  associate_public_ip_address = true
+  tags = {
+    Name       = "Server-1"
+    Env        = "Prod"
+    Owner      = "Sree"
+    CostCenter = "ABCD"
   }
 }
+
